ab5cacae92d91ae28f4961e484d53999221771ad
nathanclem100 <nathanclem100@gmail.com>
2024-12-18 19:47:18 -0600

    Recreate src folder with basic Next.js structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Next.js First Time',
+  description: 'Created with Next.js',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+} 
